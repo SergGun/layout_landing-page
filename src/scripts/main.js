c192fa09d8f8da399711d05a1da5bce6b60f5424
SergGun <wpserggun@gmail.com>
2026-01-12 02:29:44 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
+/* eslint-env browser */
 'use strict';
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(({ isIntersecting, target }) => {
+      target.classList.toggle('show', isIntersecting);
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+document
+  .querySelectorAll('.categories, .recommended')
+  .forEach((el) => observer.observe(el));
